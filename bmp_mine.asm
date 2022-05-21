e70799a9253d5c05f14573c6aceeaa6c79f0d15a
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2022-05-21 09:44:50 +0200

    width validation

---
+++ bmp_mine.asm
@@ -0,0 +1,336 @@
+#	struct {
+#		char* filename;		// wskazanie na nazwę pliku
+#		unsigned char* hdrData; // wskazanie na bufor nagłówka pliku BMP
+#		unsigned char* imgData; // wskazanie na pierwszy piksel obrazu w pamięci
+#		int width, height;	// szerokość i wysokość obrazu w pikselach
+#		int linebytes;		// rozmiar linii (wiersza) obrazu w bajtach
+#	} imgInfo;
+
+# bmp offsets declarations
+.eqv ImgInfo_file_name	0
+.eqv ImgInfo_header_bufor 	4
+.eqv ImgInfo_img_begin_ptr	8
+.eqv ImgInfo_width	12
+.eqv ImgInfo_height	16
+.eqv ImgInfo_line_bytes	20
+.eqv MAX_IMG_SIZE 	1201222 # 400 X 100 x 3 (piksele) + header 
+.eqv BMPHeader_Size 54
+.eqv BMPHeader_width 18
+.eqv BMPHeader_height 22
+
+# systen calls
+.eqv system_OpenFile	1024
+.eqv system_ReadFile	63
+.eqv system_WriteFile	64
+.eqv system_CloseFile	57
+
+
+# "variables"
+.eqv stripes_per_char 11
+.eqv pixels_per_stripe 4
+
+	.data
+imgInfo: .space	24	# deskryptor obrazu
+
+	.align 2		# wyrównanie do granicy słowa
+dummy:		.space 2
+bmpHeader:	.space	BMPHeader_Size
+
+	.align 2
+imgData: 	.space	MAX_IMG_SIZE
+
+text_to_code: .asciz "12345678ab"
+input_file_name:	.asciz "img/white400x100.bmp"
+output_file_name: .asciz "result.bmp"
+
+	.text
+main:
+	# wypełnienie deskryptora obrazu
+	la a0, imgInfo  
+	la t0, input_file_name 
+	sw t0, ImgInfo_file_name(a0) # store word
+	la t0, bmpHeader
+	sw t0, ImgInfo_header_bufor(a0)
+	la t0, imgData
+	sw t0, ImgInfo_img_begin_ptr(a0) 
+
+	jal	read_bmp
+	bnez a0, main_failure
+
+
+
+
+	jal validate_width
+	li a7, 1
+	ecall
+
+	
+	# la a0, imgInfo
+	# jal invert_red
+
+	# la a0, imgInfo
+	# la t0, output_file_name
+	# sw t0, ImgInfo_file_name(a0)
+	# jal save_bmp
+
+main_failure:
+	li a7, 10
+	ecall
+
+#============================================================================
+# read_bmp: 
+#	reads the content of a bmp file into memory
+# arguments:
+#	a0 - address of image descriptor structure
+#		input filename pointer, header and image buffers should be set
+# return value: 
+#	a0 - 0 if successful, error code in other cases
+read_bmp:
+	mv t0, a0	# preserve imgInfo structure pointer
+	
+#open file
+	li a7, system_OpenFile
+    lw a0, ImgInfo_file_name(t0)	#file name 
+    li a1, 0					#flags: 0-read file
+    ecall
+	
+	blt a0, zero, rb_error
+	mv t1, a0					# save file handle for the future
+	
+#read header
+	li a7, system_ReadFile
+	lw a1, ImgInfo_header_bufor(t0)
+	li a2, BMPHeader_Size
+	ecall
+	
+#extract image information from header
+	lw a0, BMPHeader_width(a1)
+	sw a0, ImgInfo_width(t0)
+	
+	# compute line size in bytes - bmp line has to be multiple of 4
+	add a2, a0, a0
+	add a0, a2, a0	# pixelbytes = width * 3 
+	addi a0, a0, 3
+	srai a0, a0, 2
+	slli a0, a0, 2	# linebytes = ((pixelbytes + 3) / 4 ) * 4
+	sw a0, ImgInfo_line_bytes(t0)
+	
+	lw a0, BMPHeader_height(a1)
+	sw a0, ImgInfo_height(t0)
+
+#read image data
+	li a7, system_ReadFile
+	mv a0, t1
+	lw a1, ImgInfo_img_begin_ptr(t0)
+	li a2, MAX_IMG_SIZE
+	ecall
+
+#close file
+	li a7, system_CloseFile
+	mv a0, t1
+    ecall
+	
+	mv a0, zero
+	jr ra
+	
+rb_error:
+	li a0, 1	# error opening file	
+	jr ra
+	
+
+validate_width:
+	# t1 - img witdh
+	# t0 - stripes_per_char
+	# t2 - pixels_per_stripe
+	# t4 - string length
+
+	# calc text_to_code length
+	mv t4, zero
+	la t0, text_to_code
+calc_string_len:
+	lbu t1, (t0)
+	addi t4, t4, 1
+	addi t0, t0, 1
+	bne t1, zero, calc_string_len
+	addi t4, t4, -1
+
+	la t0, imgInfo
+	lw t1, ImgInfo_width(t0)
+	li t0, stripes_per_char
+	li t2, pixels_per_stripe
+	mul t3, t0, t2 # t3 = pixels_per_stripe x stripes_per_char
+	mul t3, t3, t4 # t3 = (pixels_per_stripe x stripes_per_char) x text_len
+	sub t1, t1, t3 
+	li a0, 0
+	ble t1, zero, too_narrow_flag
+	jr ra
+
+too_narrow_flag:
+	li a0, 1
+	jr ra
+	
+
+
+
+# ============================================================================
+# save_bmp - saves bmp file stored in memory to a file
+# arguments:
+#	a0 - address of ImgInfo structure containing description of the image`
+# return value: 
+#	a0 - zero if successful, error code in other cases
+
+save_bmp:
+	mv t0, a0	# preserve imgInfo structure pointer
+	
+#open file
+	li a7, system_OpenFile
+    lw a0, ImgInfo_file_name(t0)	#file name 
+    li a1, 1					#flags: 1-write file
+    ecall
+	
+	blt a0, zero, wb_error
+	mv t1, a0					# save file handle for the future
+	
+#write header
+	li a7, system_WriteFile
+	lw a1, ImgInfo_header_bufor(t0)
+	li a2, BMPHeader_Size
+	ecall
+	
+#write image data
+	li a7, system_WriteFile
+	mv a0, t1
+	# compute image size (linebytes * height)
+	lw a2, ImgInfo_line_bytes(t0)
+	lw a1, ImgInfo_height(t0)
+	mul a2, a2, a1
+	lw a1, ImgInfo_img_begin_ptr(t0)
+	ecall
+
+#close file
+	li a7, system_CloseFile
+	mv a0, t1
+    ecall
+	
+	mv a0, zero
+	jr ra
+	
+wb_error:
+	li a0, 2 # error writing file
+	jr ra
+
+
+# ============================================================================
+# set_pixel - sets the color of specified pixel
+#arguments:
+#	a0 - address of ImgInfo image descriptor
+#	a1 - x coordinate
+#	a2 - y coordinate - (0,0) - bottom left corner
+#	a3 - 0RGB - pixel color
+#return value: none
+#remarks - a0, a1, a2 values are left unchanged
+
+set_pixel:
+	lw t1, ImgInfo_line_bytes(a0)
+	mul t1, t1, a2  # t1 = y * linebytes
+	add t0, a1, a1
+	add t0, t0, a1 	# t0 = x * 3
+	add t0, t0, t1  # t0 is offset of the pixel
+
+	lw t1, ImgInfo_img_begin_ptr(a0) # address of image data
+	add t0, t0, t1 	# t0 is address of the pixel
+	
+	#set new color
+	sb   a3,(t0)		#store B
+	srli a3, a3, 8
+	sb   a3, 1(t0)		#store G
+	srli a3, a3, 8
+	sb   a3, 2(t0)		#store R
+
+	jr ra
+
+# ============================================================================
+# get_pixel- returns color of specified pixel
+#arguments:
+#	a0 - address of ImgInfo image descriptor
+#	a1 - x coordinate
+#	a2 - y coordinate - (0,0) - bottom left corner
+#return value:
+#	a0 - 0RGB - pixel color
+#remarks: a1, a2 are preserved
+
+get_pixel:
+	lw t1, ImgInfo_line_bytes(a0)
+	mul t1, t1, a2  # t1 = y * linebytes
+	add t0, a1, a1
+	add t0, t0, a1 	# t0 = x * 3
+	add t0, t0, t1  # t0 is offset of the pixel
+
+	lw t1, ImgInfo_img_begin_ptr(a0) # address of image data
+	add t0, t0, t1 	# t0 is address of the pixel
+
+	#get color
+	lbu a0,(t0)		#load B
+	lbu t1,1(t0)		#load G
+	slli t1,t1,8
+	or a0, a0, t1
+	lbu t1,2(t0)		#load R
+    slli t1,t1,16
+	or a0, a0, t1
+					
+	jr ra
+
+# ============================================================================
+
+# ============================================================================
+# invert_red - inverts red component in the input image
+#arguments:
+#	a0 - address of ImgInfo image descriptor
+#return value:
+#	none
+
+# for (int y = imgInfo->height-1; y >= 0; --y)
+#   for (int x = imgInfo->width-1; x >= 0; --x)
+#   {
+#     unsigned rgb = get_pixel(imgInfo, x, y);
+#     rgb = (rgb & 0x0000FFFF) | (0x00FF0000 - (rgb & 0x00FF0000));
+#     set_pixel(imgInfo, x, y, rgb);
+#   }
+
+invert_red:
+	addi sp, sp, -8
+	sw ra, 4(sp)		#push ra
+	sw s1, 0(sp)		#push s1
+	mv s1, a0 			#preserve imgInfo for further use
+	
+	lw a2, ImgInfo_height(a0)
+	addi a2, a2, -1		
+	
+invert_line:
+	lw a1, ImgInfo_width(a0)
+	addi a1, a1, -1
+	
+invert_pixel:
+	jal get_pixel
+	
+	lui t0, 0x00FF0
+	and a3, a0, t0
+	sub a3, t0, a3
+	
+	slli a0, a0, 16
+	srli a0, a0, 16
+	or a3, a3, a0
+	
+	mv a0, s1
+	jal set_pixel
+	
+	addi a1, a1, -1
+	bge a1, zero, invert_pixel
+	
+	addi a2, a2, -1
+	bge a2, zero, invert_line
+	
+	lw s1, 0(sp)		#pop s1
+	lw ra, 4(sp)		#pop ra
+	addi sp, sp, 8
+	jr ra
