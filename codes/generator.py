a3850892cfd171ea8dc9b3299f59a26bd55eaf10
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2022-06-06 19:24:44 +0200

    restructure and ad readme

--- codes/generator.py
+++
@@ -1,9 +0,0 @@
-with open("code_binary.txt") as f:
-    data = f.readlines()
-
-hex_output = []
-for binary_string in data:
-    hex_output.append("\t" + str(hex(int(binary_string, 2))) + ",\n")
-
-with open("hex_output.tx", "w") as f:
-    f.writelines(hex_output)
--- dummy.asm
+++
@@ -1,6 +0,0 @@
-	# read byte word 
-    la t0, value
-    lb a0, (t0)
-    # srli a0, a0, 1 # devides by 2 - wromg way
-    li a7, 1
-    ecall
